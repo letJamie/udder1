c031744028d756d387296b896206dff6a514385f
letJamie <stillbut@naver.com>
2020-06-11 15:17:35 +0900

    Initial Commit

---
+++ udder1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder1
+//
+//  Created by Jamie on 2020/06/11.
+//  Copyright © 2020 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
